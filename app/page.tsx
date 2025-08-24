c0b42fafc5b108c592e006f6e695e845f78db09f
Patrick Chen <ptrckchen@gmail.com>
2025-08-24 13:31:34 +0900

    change spacing

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,15 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <div className="my-8">
-        <h2 className="mb-8 text-2xl font-semibold tracking-tighter">
-          About Me
-        </h2>
-        <p className="mb-4">
-          {`I am looking for some direction and this website felt like one way to 
-          try to find it. I have a tendency to ruminate so why not just let it 
-          all out here you know.`}
-        </p>
-      </div>
+      <h2 className="mb-8 text-2xl font-semibold tracking-tighter">
+        About Me
+      </h2>
+      <p className="mb-4">
+        {`I am looking for some direction and this website felt like one way to 
+        try to find it. I have a tendency to ruminate so why not just let it 
+        all out here you know.`}
+      </p>
+      
       
       <div className="my-8">
         <h2 className="mb-8 text-2xl font-semibold tracking-tighter">
