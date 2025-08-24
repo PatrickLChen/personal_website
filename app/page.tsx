a40668958b491d3215a5722c6a11b1e880304953
Patrick Chen <ptrckchen@gmail.com>
2025-08-24 16:10:55 +0900

    add horizontal lines to home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,27 @@ export default function Page() {
   return (
     <section>
       <h2 className="mb-8 text-2xl font-semibold tracking-tighter">
-        About Me
+        Patrick Chen
       </h2>
       <p className="mb-4">
         {`I am looking for some direction and this website felt like one way to 
         try to find it. I have a tendency to ruminate so why not just let it 
         all out here you know.`}
       </p>
-      
-      
+
+      <hr className="text-neutral-500 dark:text-neutral-500"/>
+
       <div className="my-8">
         <h2 className="mb-8 text-2xl font-semibold tracking-tighter">
-          My Writings
+          Loose Thoughts
         </h2>
         <BlogPosts />
       </div>
 
+      <hr className="text-neutral-500 dark:text-neutral-500"/>
       <div className="my-8">
         <h2 className="mb-8 text-2xl font-semibold tracking-tighter">
-          Other Notes
+          Collection
         </h2>
         <p className="mb-4">
           {`Other things go here`}
