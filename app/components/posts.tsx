07763c5a961a41fde89916015af740ea7e89e8f1
Patrick Chen <ptrckchen@gmail.com>
2025-08-24 15:03:52 +0900

    playing around with post formatting

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -21,12 +21,12 @@ export function BlogPosts() {
             className="flex flex-col space-y-1 mb-4"
             href={`/writing/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+            <div className="flex justify-between items-baseline">
+              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight font-bold">
                 {post.metadata.title}
+              </p>
+              <p className="text-neutral-600 dark:text-neutral-400 tabular-nums whitespace-nowrap ml-4">
+                {formatDate(post.metadata.publishedAt, false)}
               </p>
             </div>
           </Link>
